3a77126fb6b44e54c94d84487743a23ef40f56db
Casey Hebebrand <caseyhebebrand@gmail.com>
2017-10-24 19:07:15 -0700

    refactor schema

--- analytics/schema.sql
+++ analytics/schema.sql
@@ -4,46 +4,33 @@ CREATE DATABASE analytics;
 USE analytics;
 
 CREATE TABLE user_data (
-  id INT NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
+  userId INT NOT NULL,
   ratio DECIMAL(5, 4) NOT NULL,
   engagement DECIMAL(5, 4),
-  category VARCHAR(200),
+  first VARCHAR(200),
+  second VARCHAR(200),
+  third VARCHAR(200),
   createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
   PRIMARY KEY (id)
 );
 
-CREATE TABLE categories (
+CREATE TABLE interests (
   id INT AUTO_INCREMENT NOT NULL,
   name VARCHAR(200) NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE user_categories (
+CREATE TABLE user_inputs (
   id INT AUTO_INCREMENT NOT NULL,
-  userId INT NOT NULL,
+  usersId INT NOT NULL,
   categoryId INT NOT NULL,
   clicks INT,
   createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id),
-  FOREIGN KEY (userId) REFERENCES user_data(id),
-  FOREIGN KEY (categoryId) REFERENCES categories(id)
+  FOREIGN KEY (categoryId) REFERENCES interests(id)
 );
 
-CREATE TABLE fake_client (
-  userId INT,
-  engagement DECIMAL(5, 4),
-  calcualte TINYINT(1),
-  food INT,
-  fashion INT, 
-  products INT,
-  travel INT,
-  events INT,
-  design INT,
-  entertainment INT,
-  crafts INT,
-  photography INT
-);
-
-INSERT INTO categories (name) VALUES 
+INSERT INTO interests (name) VALUES 
   ("food"), ("fashion"), ("products"), ("sports"), ("travel"),
   ("events"), ("design"), ("entertainment"), ("DIY/crafts"), ("photography");
