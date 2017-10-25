9380d6b43a532d6e3144046fcac32f1677fedb9f
Casey Hebebrand <caseyhebebrand@gmail.com>
2017-10-25 16:17:55 -0700

    update dashboard script

--- analytics/dashboard/index.js
+++ analytics/dashboard/index.js
@@ -1,6 +1,17 @@
 const elasticsearch = require('elasticsearch');
 const client = new elasticsearch.Client({
   host: 'localhost:9200',
+  log: 'trace',
+});
+
+client.ping({
+  requestTimeout: 30000,
+}, function (error) {
+  if (error) {
+    console.error('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
 });
 
 const submitInputIndex = (userInputs) => {
