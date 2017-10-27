7934479c23ceb8e50de5a801ad82453eb82192ae
Casey Hebebrand <caseyhebebrand@gmail.com>
2017-10-26 19:12:01 -0700

    update server logic for POST

--- analytics/routes/analyze.js
+++ analytics/routes/analyze.js
@@ -7,12 +7,48 @@ const analysis = require('../analysis/ratioRegression.js');
 router.post('/', (req, res) => {
   console.log('in ANALYZE ROUTER', req.body, req.body.adClicks);
   const inputs = req.body;
+  const userId = inputs.userId;
+  const engagement = inputs.engagement;
   const params = [];
+  const outputs = {
+    userId: userId,
+    interests: [],
+  };
   for (const key in inputs.adClicks) {
     params.push(inputs.adClicks[key]);
   }
+
   dashboard.submitInputIndex(inputs);
-  db.insertAdClicks(inputs.userId, params);
+  
+  db.insertAdClicks(userId, params)
+    .then(() => {
+      if (!inputs.scoreDropped) {
+        throw 
+      } else {
+        return analysis.calculateRatio(userId, engagement);
+      }
+    })
+    .then((ratio) => {
+      outputs.ratio = ratio;
+      return db.getTopAdInterests(userId);
+    })
+    .then((interests) => {
+      const param = [userId, outputs.ratio, engagement];
+      interests.forEach((interest) => {
+        outputs.interests.push(interest.name);
+        param.push(interest.categoryId);
+      });
+      return db.insertNewData(param);
+    })
+    .then(() => {
+      // send to ad aggregator
+      res.status(200).send();
+    })
+    .catch(() => {
+      res.status(200).send();
+    });
+
+
   analysis.calculateRatio(inputs.userId, inputs.engagementScore)
     .then((ratio) => {
       console.log('ratio', ratio);
