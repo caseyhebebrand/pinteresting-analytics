f5d7382a688308757eb4cbb23fe6c1d8f405e71d
Casey Hebebrand <caseyhebebrand@gmail.com>
2017-10-31 12:35:14 -0700

    set up connection to message bus

--- client_module/index.js
+++ client_module/index.js
@@ -1,26 +1,41 @@
 const express = require('express');
 const data = require('./dataGenerator.js');
 const request = require('request-promise');
+const worker = require('./worker.js');
+const queueUrl = require('../config.js').QUEUE_URL;
 
 const app = express();
 const PORT = 3001;
 
+// const sendData = () => {
+//   const body = data.userLogOut();
+//   const options = {
+//     method: 'POST',
+//     url: 'http://127.0.0.1:3000/analyze',
+//     body,
+//     json: true,
+//   };
+//   request(options)
+//     .then(results => console.log('success sending data to analytics', results))
+//     .catch(err => console.error('post to analytics module failed', err));
+
+// };
+
 const sendData = () => {
   const body = data.userLogOut();
-  const options = {
-    method: 'POST',
-    url: 'http://127.0.0.1:3000/analyze',
-    body,
-    json: true,
+  const params = {
+    MessageBody: JSON.stringify(body),
+    QueueUrl: queueUrl,
+    DelaySeconds: 0,
   };
-  request(options)
-    .then(results => console.log('success sending data to analytics', results))
-    .catch(err => console.error('post to analytics module failed', err));
 
+  worker.send(params)
+    .then(results => console.log('success sending to SQS', results))
+    .catch(err => console.error('send to SQS failed', err));
 };
 
-//sendData();
-setInterval(sendData, 100);
+sendData();
+//setInterval(sendData, 100);
 
 app.listen(PORT, (err) => {
   if (err) {
