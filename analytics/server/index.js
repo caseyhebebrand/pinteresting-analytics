5ba5c604121f59a3ce1bd537cabf0c2e5ed239bd
Casey Hebebrand <caseyhebebrand@gmail.com>
2017-11-01 12:44:02 -0700

    code clean-up

--- analytics/server/index.js
+++ analytics/server/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const db = require('../database/index.js');
 const router = require('../routes/analyze.js').router;
 const cluster = require('cluster');
 const cpuCount = require('os').cpus().length;
@@ -17,10 +16,8 @@ if (cluster.isMaster) {
   module.exports.app = app;
   const PORT = 3000;
   app.use(bodyParser.json());
-  
   // set up routes
   app.use('/analyze', router);
-  
   app.listen(PORT, (err) => {
     if (err) {
       console.log('cannot connect to server', err);
--- analytics/workers/getClientInput.js
+++ analytics/workers/getClientInput.js
@@ -16,39 +16,6 @@ const consumer = Consumer.create({
   sqs: new AWS.SQS(),
 });
 
-
-// Instantiate SQS
-// const sqs = new aws.SQS();
-// const receiveSQS = (params) => {
-//   return new Promise((resolve, reject) => {
-//     sqs.receiveMessage(params, (err, data) => {
-//       if (err) {
-//         //console.log('error receiving')
-//         reject(err);
-//       } else {
-//         //console.log('success receiving')
-//         resolve(data);
-//       }
-//     });
-//   });
-// };
-// const deleteSQS = (params) => {
-//   return new Promise((resolve, reject) => {
-//     sqs.deleteMessage(params, (err, data) => {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         resolve(data);
-//       }
-//     });
-//   });
-// };
-
-// module.exports = {
-//   receiveSQS,
-//   deleteSQS,
-// };
-
 module.exports = {
   consumer,
 };
