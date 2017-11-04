a121f2d238c6788e626f80226b82350f0e201a94
Casey Hebebrand <caseyhebebrand@gmail.com>
2017-11-04 16:41:59 -0700

    temp add config files

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  INPUT_QUEUE_URL: 'https://sqs.us-west-2.amazonaws.com/470758207750/clientInput',
+  OUTPUT_QUEUE_URL: 'https://sqs.us-west-2.amazonaws.com/470758207750/analyticsOutput',
+  MYSQL_HOST: 'pinteresting.c7y1wgarfmf4.us-west-1.rds.amazonaws.com',
+  MYSQL_PORT: 3306,
+  MYSQL_USERNAME: 'caseyhebebrand',
+  MYSQL_PASSWORD: 'analytics',
+  MYSQL_DATABASE: 'analytics',
+  ES_HOST: 'https://search-pinterestinganalytics-kidsizmu32stczhjpmos4ymekq.us-west-2.es.amazonaws.com',
+};
