5b3373f1f7f2676dc55123728a23090b905d8e5d
Casey Hebebrand <caseyhebebrand@gmail.com>
2017-11-04 11:37:50 -0700

    prep aggregator with process envs

--- aggregator_module/worker.js
+++ aggregator_module/worker.js
@@ -1,12 +1,19 @@
 const AWS = require('aws-sdk');
 const Consumer = require('sqs-consumer');
 const queueUrl = require('../config.js').OUTPUT_QUEUE_URL;
+const awsAccess = require('../config.json');
 
 // load aws credentials
-AWS.config.loadFromPath(__dirname + '/config.json');
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID || awsAccess.accessKeyId,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ||awsAccess.secretAccessKey,
+  region: process.env.AWS_SQS_REGION || awsAccess.region,
+});
+// Can use below on local env
+//AWS.config.loadFromPath(__dirname + '/config.json');
 
 const consumer = Consumer.create({
-  queueUrl,
+  queueUrl: process.env.ES_HOST || queueUrl,
   waitTimeSeconds: 10,
   handleMessage: (message, done) => {
     console.log('AD got the message', message)
